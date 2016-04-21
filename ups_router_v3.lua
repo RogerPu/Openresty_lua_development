e1a414cdc04f578cc86c4e2b1f9f62c1b00580ca
RogerPu <espwj@126.com>
2016-04-21 14:54:49 +0800

    update

--- ups_router_v3.lua
+++ ups_router_v3.lua
@@ -4,40 +4,111 @@ local headers = ngx.req.get_headers()
 local apikey = headers["apikey"]
 local redis_host = '127.0.0.1'
 local redis_port = 6379
-
+local resty_lock = require "resty.lock"
+local cache  = ngx.shared.share_dict_cache
 local redis = require "resty.redis"
-local red = redis:new()
---redis.add_commands("keys")
-red:set_timeout(1000)  -- 1 second
 
-local ok,err = red:connect(redis_host , redis_port)
-if not ok then 
-	ngx.say("failed to connect upstream configure Redis: ", err)
-	return
+local function fetch_redis(redis_host,redis_port,key)
+
+	local red = redis:new()
+	red:set_timeout(1000)  -- 1 second
+
+	local ok,err = red:connect(redis_host , redis_port)
+	if not ok then 
+		return fail("failed to connect upstream configure Redis: ", err)
+	end
+
+	local value,err = red:get(key)
+	if not value then 
+		return fail("failed to get upstream: ",err)
+	elseif value = ngx.null then
+		return fail("key not found,",err)
+	end
+	return value
+
 end
+
+
+
+local function get_upstream(apikey)
+
+	--SHM cache process
+	--step 1:
+	local upstream_name,err = cache:get(apikey)
+	if upstream_name then 
+		ngx.var.target = upstream_name
+		return
+	end
+
+	if err then 
+		return fail("failed to get key from shm:",err)
+	end
+
+	--cache miss!
+	--step 2, lock the key:	
+	local lock = resty_lock:new("my_locks")
+	local elapsed,err = lock:lock(apikey)
+	if not elapsed then
+		return fail("failed to acquire the lock: ",err)
+	end
+	--lock successfully acquired!
+
+	--step3:
+	--someone might have already put the upstream_nameue into the cache
+	--so check it here again:
+	upstream_name,err = cache:get(apikey)
+	if upstream_name then
+		local ok,err = lock:unlock()
+		if not ok then
+			return fail("failed to unlock: ",err)
+		end
+		ngx.var.target = upstream_name
+		return
+	end
+	--step4:
+
+	local upstream_name = fetch_redis(redis_host,redis_port,apikey)
+	if not upstream_name then
+		local ok,err = lock:unlock()
+		if not ok then
+			return fail("failed to unlock: ",err)
+		end
+		ngx.say("no value found in redis")
+		return 
+	end
+
+	--update the shm cache with the newly fetched value
+	local ok,err = cache:set(apikey,upstream_name,1)
+	if not ok then 
+		local ok,err = lock:unlock()
+		if not ok then
+			return fail("failed to unlock: ",err)
+		end
+		return fail("failed to update shm cache: ",err)
+	end
+
+	local ok.err = lock:unlock()
+	if not ok then 
+		return fail("failed to unlock",err)
+	end
+end
+
 
 --Check if request has apikey in the headers
-if apikey == nil then
+if not apikey then
 	ngx.say("Please Offer apikey in the headers")
-end
+end	
 
---Macth the header and get the right upstream and return to Nginx
-local function check_apikey_match (apikey)
+		
 
-	local apikey_map_value,err = red:get(apikey)		
-	
-	if not apikey_map_value then 
-		ngx.say("failed to get upstream",":",v, err)
-		return
 
-	elseif apikey_map_value == ngx.null then 
-		ngx.say("upstream not found.")
-		return			
-	end
-	ngx.var.target = apikey_map_value				
+
+
+
+
 			
-end
-check_apikey_match(apikey)
+	
+
 
 
 
